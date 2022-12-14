8d7d5e6f4330a1903d71531d4e4665ded09c072b
yesjm <wudals0708@naver.com>
2022-12-14 16:52:36 +0900

    feat: spring security project init

---
+++ src/main/kotlin/com/cos/security1/Security1Application.kt
@@ -0,0 +1,11 @@
+package com.cos.security1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Security1Application
+
+fun main(args: Array<String>) {
+    runApplication<Security1Application>(*args)
+}
